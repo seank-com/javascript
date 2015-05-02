c5f355cc6a87f36642f55841610ce705fcb14e74
Sean Kelly <home@seank.com>
2015-05-02 13:31:44 -0700

    Now supports Visual Studio 12 and Atom!

--- init.cmd
+++ init.cmd
@@ -1,7 +1,8 @@
 @echo off
-set PATH=%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin;C:\Program Files\Debugging Tools for Windows (x64);C:\Python27;C:\Program Files\nodejs\;C:\Users\SeanK\AppData\Roaming\npm;%~dp0
+set PATH=%PATH%;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin;C:\Program Files\Debugging Tools for Windows (x64);C:\Python27;C:\Program Files\nodejs\;C:\Users\SeanK\AppData\Roaming\npm;C:\Users\SeanK\AppData\Local\atom\bin;%~dp0
 set NODE_PATH=C:\Users\SeanK\AppData\Roaming\npm\node_modules
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 alias -f "C:\Program Files (x86)\Scripts\cmds.lst"
 start keys.ahk
 pushd "%~dp0"
