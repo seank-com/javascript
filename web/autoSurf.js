9ed8d1c0a024c51b48cac59629eb5fad4a1c93d7
Sean Kelly <home@seank.com>
2016-10-02 10:44:57 -0700

    Fix formatting

--- web/autoSurf.js
+++ web/autoSurf.js
@@ -172,64 +172,58 @@ var http = require('http'),
     //  outputs input urls that failed to download
     //
     operationDownload = function (operation, callback) {
-        "use strict";
-
         var downloads = 0,
-            processDownload = function (err, result) {
-
-                if (err) {
                     operation.output.push(result.url);
-                    console.log('ERROR downloading: ' + result.url + '\n          message: ' + err.message);
-                } else {
-                    console.log('Downloaded (' + downloads + ' remaining): ' + result.url);
-                }
-
-                downloads -= 1;
-
-                if (downloads === 0) {
-                    callback(operation);
-                }
-            },
-            startDownload = function (element) {
-
-                var re = new RegExp("^(.*)$"),
-                    matches = [],
-                    filename = '',
-                    directory = path.resolve('.', '.');
-
-                if (operation.pattern) {
-
-                    if (operation.pattern[0] !== '^' || operation.pattern[operation.pattern.length - 1] !== '$') {
-                        console.log('ERROR: pattern must begin with ^ and end with $');
-                        return;
-                    }
-
-                    re = new RegExp(operation.pattern);
-                }
-
-                matches = element.match(re);
-                if (operation.filename) {
                     filename = operation.filename.format(matches);
-                } else {
-                    filename = element.substr(element.lastIndexOf('/') + 1);
-                }
-
-                if (operation.directory) {
                     directory = operation.directory.format(matches);
-                }
-
-                filename = path.resolve(directory, filename);
-
-                if (operation.debug) {
-                    console.log('directory: ' + directory);
-                    console.log('filename : ' + filename);
-                } else {
-                    downloadFile(element, filename, processDownload);
-                }
-            };
-
         downloads = operation.input.length;
         operation.input.forEach(startDownload);
+      "use strict";
+
+        processDownload = function (err, result) {
+          if (err) {
+            console.log('ERROR downloading: ' + result.url + '\n          message: ' + err.message);
+          } else {
+            console.log('Downloaded (' + downloads + ' remaining): ' + result.url);
+          }
+          downloads -= 1;
+          if (downloads === 0) {
+            callback(operation);
+          }
+        },
+        startDownload = function (element) {
+          var re = new RegExp("^(.*)$"),
+              matches = [],
+              filename = '',
+              directory = path.resolve('.', '.');
+
+          if (operation.pattern) {
+            if (operation.pattern[0] !== '^' || operation.pattern[operation.pattern.length - 1] !== '$') {
+              console.log('ERROR: pattern must begin with ^ and end with $');
+              return;
+            }
+            re = new RegExp(operation.pattern);
+          }
+
+          matches = element.match(re);
+
+          if (operation.filename) {
+          } else {
+            filename = element.substr(element.lastIndexOf('/') + 1);
+          }
+
+          if (operation.directory) {
+          }
+
+          filename = path.resolve(directory, filename);
+
+          if (operation.debug) {
+            console.log('directory: ' + directory);
+            console.log('filename : ' + filename);
+          } else {
+            downloadFile(element, filename, processDownload);
+          }
+        };
     },
     //
     // Downloads all the input urls and parses the HTML for
@@ -246,29 +240,27 @@ var http = require('http'),
         "use strict";
 
         var pages = 0,
-            processPage = function (err, result) {
+          processPage = function (err, result) {
 
-                if (err) {
-                    console.log('ERROR parsing: ' + result.sourceUrl + '\n      message: ' + err.message);
-                } else {
                     operation.output = operation.output.concat(result.urlsFound);
-                    console.log('Parsed (' + pages + ' remaining): ' + result.sourceUrl);
-                }
-
-                pages -= 1;
-
-                if (pages === 0) {
-                    if (operation.debug) {
                         console.log(pruneDuplicates(operation.output));
-                    }
+            if (err) {
+              console.log('ERROR parsing: ' + result.sourceUrl + '\n      message: ' + err.message);
+            } else {
+              console.log('Parsed (' + pages + ' remaining): ' + result.sourceUrl);
+            }
 
-                    callback(operation);
-                }
-            },
-            startFetch = function (element) {
+            pages -= 1;
 
-                parseHtml(element, processPage);
-            };
+            if (pages === 0) {
+              if (operation.debug) {
+              }
+              callback(operation);
+            }
+          },
+          startFetch = function (element) {
+            parseHtml(element, processPage);
+          };
 
         pages = operation.input.length;
         operation.input.forEach(startFetch);
@@ -327,6 +319,11 @@ var http = require('http'),
             callback(operation);
         });
     },
+    //
+    // Generates urls for each pattern in patterns
+    // replacing {0} with values from start increased by
+    // increment until the value exceeds end.
+    //
     //  {
     //    "operation": "generate",
     //    "input": [url, ...],
@@ -338,31 +335,30 @@ var http = require('http'),
     //  }
     //
     operationGenerate = function (operation, callback) {
-        "use strict";
-
-        var patterns = operation.patterns || [],
-            count = patterns.length,
-            start = operation.start || 0,
-            increment = operation.increment || 1,
-            end = operation.end || 0,
-            i = 0,
             value = 0;
-
-        operation.output = operation.input;
-
-        for (i = 0; i < count; i += 1) {
-            for (value = start; value <= end; value += increment) {
                 operation.output.push(operation.patterns[i].format(value));
-            }
-        }
+      "use strict";
 
         if (operation.debug) {
             console.log(pruneDuplicates(operation.output));
-        }
+      var patterns = operation.patterns || [],
+        count = patterns.length,
+        start = operation.start || 0,
+        increment = operation.increment || 1,
+        end = operation.end || 0,
+        i = 0,
+        value = 0,
 
-        process.nextTick(function () {
-            callback(operation);
-        });
+      operation.output = operation.input;
+
+      for (i = 0; i < count; i += 1) {
+        for (value = start; value <= end; value += increment) {
+        }
+      }
+
+      process.nextTick(function () {
+        callback(operation);
+      });
     },
     //
     // Copies input to output. If inputFilename is
@@ -385,128 +381,126 @@ var http = require('http'),
         "use strict";
 
         var processWrite = function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              callback(operation);
+            }
+          },
+          saveFile = function() {
+            var filename = '',
+              data = '';
 
-                if (err) {
-                    console.log(err);
-                } else {
-                    callback(operation);
-                }
-            },
-            saveFile = function() {
+            if (operation.outputFilename) {
+              filename = path.resolve('.', operation.outputFilename);
+              fs.writeFile(filename, data, processWrite);
+            } else {
+              process.nextTick(function () {
+                callback(operation);
+              });
+            }
+          },
+          processRead = function (err, data) {
+            var loaded = {},
+              i = 0;
 
-                var filename = '',
-                    data = '';
-
-                if (operation.outputFilename) {
-                    filename = path.resolve('.', operation.outputFilename);
                     data = JSON.stringify(pruneDuplicates(operation.output));
-                    fs.writeFile(filename, data, processWrite);
-                } else {
-                    process.nextTick(function () {
-                        callback(operation);
-                    });
-                }
-            },
-            processRead = function (err, data) {
-
-                var loaded = [],
                     count = 0,
-                    i = 0;
-
-                if (err && err.code !== 'ENOENT') {
-                    console.log(err);
-                } else {
-                    if (!err) {
-                        loaded = JSON.parse(data);
-
                         count = loaded.length;
                         for(i = 0; i < count; i += 1) {
                             operation.output.push(loaded[i]);
                         }
                     }
 
-                    saveFile();
+            if (err && err.code !== 'ENOENT') {
+              console.log(err);
+            } else {
+              if (!err) {
+                loaded = JSON.parse(data);
+
                 }
-            },
-            loadFile = function() {
 
-                var filename = '';
-
-                if (operation.inputFilename) {
-                    filename = path.resolve('.', operation.inputFilename);
-                    fs.readFile(filename, { 'encoding': "utf8" }, processRead);
-                } else {
-                    saveFile();
                 }
-            };
+              saveFile();
+          },
+          loadFile = function() {
+            var filename = '';
 
+            if (operation.inputFilename) {
+              filename = path.resolve('.', operation.inputFilename);
+              fs.readFile(filename, { 'encoding': "utf8" }, processRead);
+            } else {
+              saveFile();
+          };
         operation.output = operation.input;
-
         loadFile();
     },
     operations = [],
     doNextOperation = function (operation) {
-        "use strict";
+      "use strict";
 
         var nextOperation = operations.pop(),
             input = [];
 
-        if (operation) {
             input = pruneDuplicates(operation.output);
-        }
+      if (operation) {
+      }
 
-        if (nextOperation) {
             if (nextOperation.input && nextOperation.input.length > 0) {
                 input = input.concat(nextOperation.input);
             }
-            nextOperation.input = input;
             nextOperation.output = nextOperation.output || [];
             nextOperation.output = nextOperation.output || [];
-            nextOperation.operation = nextOperation.operation || '';
-            nextOperation.debug = nextOperation.debug || false;
-
-            switch (nextOperation.operation) {
-            case 'download':
-                operationDownload(nextOperation, doNextOperation);
-                break;
-            case 'parse':
-                operationParse(nextOperation, doNextOperation);
-                break;
-            case 'filter':
-                operationFilter(nextOperation, doNextOperation);
-                break;
-            case 'generate':
-                operationGenerate(nextOperation, doNextOperation);
-                break;
-            case 'IO':
-                operationIO(nextOperation, doNextOperation);
-                break;
             }
         } else {
             if (input.length > 0) {
                 console.log(input);
+      if (nextOperation) {
             }
         }
+
+        nextOperation.input = input;
+        nextOperation.operation = nextOperation.operation || '';
+        nextOperation.debug = nextOperation.debug || false;
+
+        switch (nextOperation.operation) {
+        case 'download':
+          operationDownload(nextOperation, doNextOperation);
+          break;
+        case 'parse':
+          operationParse(nextOperation, doNextOperation);
+          break;
+        case 'filter':
+          operationFilter(nextOperation, doNextOperation);
+          break;
+        case 'tag':
+          operationTag(nextOperation, doNextOperation);
+          break;
+        case 'generate':
+          operationGenerate(nextOperation, doNextOperation);
+          break;
+        case 'IO':
+          operationIO(nextOperation, doNextOperation);
+          break;
     },
     main = function (argc, argv) {
-        "use strict";
+      "use strict";
 
-        if (argc < 3) {
-            console.log('usage: %s %s <filename>', argv[0], argv[1]);
-        } else {
-            getOperationChain(path.resolve('.', argv[2]), function (err, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    operations = result.reverse();
-
-                    process.nextTick(function () {
-                        doNextOperation(null);
-                    });
-                }
+      if (argc < 3) {
+        console.log('usage: %s %s <filename>', argv[0], argv[1]);
+      } else {
+        getOperationChain(path.resolve('.', argv[2]), function (err, result) {
+          if (err) {
+            console.log(err);
+          } else {
+            operations = result.reverse();
             dbg("DBG: Loaded operations", operations);
+            process.nextTick(function () {
+                doNextOperation(null);
             });
         }
+        });
+      }
     },
     argv = process.argv,
     argc = argv.length;
