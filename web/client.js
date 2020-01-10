ffcc11b9831b2342cd621113d28e64e7937c8811
Sean Kelly <home@seank.com>
2020-01-10 09:40:18 -0800

    making it look more professional

--- web/client.js
+++ web/client.js
@@ -1,28 +1,28 @@
 const crypto = require('crypto');
-const http = require('http');
+const https = require('https');
+const access_name = 'name';
+const access_key = 'secret';
+const hostname = 'api.endpoint.com';
 
-function getUsers() {
+function getRobots() {
 
     var options = {
-        hostname: 'localhost',
-        port: 3000,
-        path: '/user',
+        hostname: hostname,
+        path: '/api/base/robots',
         method: 'GET',
         headers: {
         }
     },
     dt = new Date(Date.now()).toUTCString(),
-    access_name = "test_name",
-    access_key = "test_key",
-    hmac = crypto.createHmac("sha1", access_key),
-    string_to_sign = options.method + " " + options.path + "\\n" + dt;
+    hmac = crypto.createHmac('sha1', access_key),
+    string_to_sign = options.method + ' ' + options.path + '\\n' + dt;
 
-    hmac.update(string_to_sign, "utf8");
+    hmac.update(string_to_sign, 'utf8');
 
-    options.headers["Date"] = dt;
-    options.headers["Authorization"] = "SEGWAY " + access_name + ":" + hmac.digest('base64')
+    options.headers['Date'] = dt;
+    options.headers['Authorization'] = 'SEGWAY ' + access_name + ':' + hmac.digest('base64')
 
-    var req = http.request(options, (res) => {
+    var req = https.request(options, (res) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
             console.log(`BODY: ${chunk}`);
@@ -39,4 +39,39 @@ function getUsers() {
     req.end();
 }
 
-getUsers();
+function getRobot(robotId) {
+
+    var options = {
+        hostname: hostname,
+        path: '/api/base/robot/' + robotId + '/status',
+        method: 'GET',
+        headers: {
+        }
+    },
+    dt = new Date(Date.now()).toUTCString(),
+    hmac = crypto.createHmac('sha1', access_key),
+    string_to_sign = options.method + ' ' + options.path + '\\n' + dt;
+
+    hmac.update(string_to_sign, 'utf8');
+
+    options.headers['Date'] = dt;
+    options.headers['Authorization'] = 'SEGWAY ' + access_name + ':' + hmac.digest('base64')
+
+    var req = https.request(options, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+  
+    req.end();
+}
+
+getRobot('EVT6-2-8');
